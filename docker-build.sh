b213790e2b19302648124d5b7d19017833b67b65
Samuel Eklund <sam@llnl.gov>
2018-08-09 08:48:21 -0700

    Moved Dockerfiles into individual folders and renamed each to Dockerfile (for syntax highlighting and improved build context handling)

--- docker-build.sh
+++ docker-build.sh
@@ -13,9 +13,10 @@
 
 # Build docker images
 docker build -t mstk-${OS}-base \
-    -f ${DOCKERFILE_FOLDER}/Dockerfile.${OS} .
+    -f ${DOCKERFILE_FOLDER}/${OS}/Dockerfile .
 
 docker build --rm -t mstk-${OS} \
     --build-arg OS=$OS \
-    --build-arg TOOLKIT_VERSION=1.4.0 \
-    -f ${DOCKERFILE_FOLDER}/Dockerfile.mstk .
+    --build-arg TOOLKIT_VERSION=${TOOLKIT_VERSION} \
+    --build-arg REQUIREMENTS_FILE=${REQUIREMENTS_FILE} \
+    -f ${DOCKERFILE_FOLDER}/mstk/Dockerfile .
