5b54d1f1a135ee40764dba6da47ce50fb0d828e4
Samuel Eklund <sam@llnl.gov>
2018-08-10 12:08:06 -0700

    Added flags to docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -9,8 +9,34 @@
 # built in docker-build.sh.                                         #
 #####################################################################
 
+usage()
+{
+    echo -e "\n-o | --operating-system"
+    echo "  Available operating systems:"
+    echo "  - centos"
+    echo "  - debian"
+    echo "  - ubuntu_14_04"
+    echo -e "  - ubuntu_16_04 (default)\n"
+    echo "-h | --help"
+    echo -e "  Show this help message.\n"
+}
 
 # Set env vars
 . ./env-vars.sh
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -o | --operating-system )
+            shift
+            OS=$1
+            ;;
+        -h | --help )
+            shift
+            usage
+            exit
+            ;;
+    esac
+    shift
+done
 
 docker run --rm mstk-$OS
